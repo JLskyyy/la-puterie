fb9e152f39830ed5a20463c105f51739db74db54
JLskyyy <clemlfrs763006@gmail.com>
2023-01-14 12:51:44 +0100

    Bot 0.1

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: 'MTA2MzYwMjA3MTAzNjc2ODM0OA.GeO027.V3W_DKhkxFuE9oaAVPIdcKn2HgDyxbQBU75QNQ',
+        owners: '945759143178944602', //id buyer
+        funny: '945759143178944602', //id Développeur
+        color: 'FF3333', //couleur par défaut
+        footer: '[ + ] La Puterie 🏴#3840',
+        maxVol: '150',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
